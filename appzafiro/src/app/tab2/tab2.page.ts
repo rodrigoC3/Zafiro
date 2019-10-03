d5d86c247683fb139af710510ed77e6bacb887d0
JulianR <julianrom.co@gmail.com>
2019-10-02 20:48:29 -0500

    Proyecto zafiro 1

---
+++ appzafiro/src/app/tab2/tab2.page.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
+  constructor() {}
+
+}
